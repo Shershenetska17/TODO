df457ef3aad847cf98eb00e2e29a419fe931e972
Shershenetska17 <154521240+Shershenetska17@users.noreply.github.com>
2025-04-30 22:15:38 +0200

    Update setup.php

--- setup.php
+++ setup.php
@@ -1,1 +1,26 @@
+<?php
+$host = "lizadb2server.mysql.database.azure.com";
+$username = "lizaadmin@lizadb2server";
+$password = "Llll1234Z";
+
+$conn = new mysqli($host, $username, $password);
+
+if ($conn->connect_error) {
+    die("Błąd połączenia: " . $conn->connect_error);
+}
+
+$sql = "
+CREATE USER 'liza2'@'%' IDENTIFIED WITH mysql_native_password BY 'Llll1234Z';
+GRANT ALL PRIVILEGES ON *.* TO 'liza2'@'%';
+FLUSH PRIVILEGES;
+";
+
+if ($conn->multi_query($sql) === TRUE) {
+    echo "✅ Użytkownik 'liza2' został utworzony!";
+} else {
+    echo "❌ Błąd: " . $conn->error;
+}
+
+$conn->close();
+?>
 
