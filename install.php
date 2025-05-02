c7635ee1fc3f6fdb1c4cb9583a33e410343fc07a
Shershenetska17 <154521240+Shershenetska17@users.noreply.github.com>
2025-05-02 18:16:52 +0200

    Update install.php

--- install.php
+++ install.php
@@ -3,14 +3,16 @@ $host = "liza-db17.mysql.database.azure.com";
 $username = "lizaadmin@liza-db17";
 $password = "Liza1234Z";
 
-// Połączenie bez wskazania konkretnej bazy (bo dopiero ją tworzymy)
+// Tworzymy połączenie bez wskazania konkretnej bazy danych
 $conn = new mysqli($host, $username, $password);
 
+// Sprawdzenie połączenia
 if ($conn->connect_error) {
     die("❌ Błąd połączenia: " . $conn->connect_error);
 }
 
-$sql = "CREATE DATABASE todo";
+// Tworzymy bazę danych
+$sql = "CREATE DATABASE IF NOT EXISTS todo";
 
 if ($conn->query($sql) === TRUE) {
     echo "✅ Baza danych 'todo' została utworzona!";
