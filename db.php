3a4777bf781222c1a33178207df8ed24837fcbc7
Shershenetska17 <154521240+Shershenetska17@users.noreply.github.com>
2025-05-02 18:39:05 +0200

    Update db.php

--- db.php
+++ db.php
@@ -2,10 +2,12 @@
 $host = "liza-db17.mysql.database.azure.com";
 $username = "lizaadmin@liza-db17";
 $password = "Liza1234Z";
-$dbname = "todo"; // zakładamy, że tak się będzie nazywać Twoja baza
+$dbname = "todo";
 
-$conn = new mysqli($host, $username, $password, $dbname);
+// Połączenie z bazą danych na porcie 3306
+$conn = new mysqli($host, $username, $password, $dbname, 3306);
 
+// Sprawdzenie połączenia
 if ($conn->connect_error) {
     die("❌ Błąd połączenia z bazą danych: " . $conn->connect_error);
 }
