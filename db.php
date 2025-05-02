16ec93d7a93bd4be9597631d5b742058ba6e0169
Shershenetska17 <154521240+Shershenetska17@users.noreply.github.com>
2025-05-02 18:44:30 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,11 @@
 <?php
 $host = "liza-db17.mysql.database.azure.com";
-$username = "lizaadmin@liza-db17";
+$username = "lizaadmin"; // ← bez @liza-db17
 $password = "Liza1234Z";
 $dbname = "todo";
 
-// Połączenie z bazą danych na porcie 3306
 $conn = new mysqli($host, $username, $password, $dbname, 3306);
 
-// Sprawdzenie połączenia
 if ($conn->connect_error) {
     die("❌ Błąd połączenia z bazą danych: " . $conn->connect_error);
 }
